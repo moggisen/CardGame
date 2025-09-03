899f59f5bbf25e2f0d78e00a5df6f778e8882e60
Emil Asikainen <emilasikainen@hotmail.com>
2025-09-03 12:17:54 +0200

    added gamestate function

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 import { drawCard} from './cardGame'
+import type { GameState } from './cardGame';
 
 // Fetch Element
 const app = document.querySelector<HTMLDivElement>('#app')!;
@@ -7,8 +8,16 @@ const app = document.querySelector<HTMLDivElement>('#app')!;
 // Draw a card
 const card = drawCard();
 
+let gameState: GameState = "waiting";
+
+//Start game
+gameState = "playing";
+
+//End game
+gameState = "ended";
+
 // Show the card
 app.innerHTML = `
-  <h1>You Drew a card!</h1>
-  <p>${card.rank} of ${card.suit}</p>
+  <h2>Game State: ${gameState}</h2>
+  <p>you drew: ${card.rank} ${card.suit}</p>
 `
